1dc9c9f2b71f75c18f437aa27a0bbb4eb2b57507
klarahapkova <80477797+klarahapkova@users.noreply.github.com>
2021-04-24 13:42:21 +0200

    Třetí úkol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import './style.css';
+import "./style.css";
 
-console.log('funguju!');
+console.log("funguju!");
+
+const buttonElm = document.querySelector("#nav__btn");
+const navElm = document.querySelector("#nav");
+const navItems = document.querySelectorAll("#nav__items");
+
+buttonElm.addEventListener("click", () => {
+  navElm.classList.toggle("nav-closed");
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    navElm.classList.toggle("nav-closed");
+  });
+});
